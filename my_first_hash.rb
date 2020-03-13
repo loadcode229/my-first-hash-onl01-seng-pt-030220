2c5ca28383d339335a845fecb9a6890b9778f546
Adam Priest <sepone229@gmail.com>
2020-03-13 10:26:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,12 +1,9 @@
 def my_hash
   my_hash = {"Python" => "data", "C++" => "games"}
 end
-
-
 def shipping_manifest
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
-
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5, 
@@ -15,7 +12,6 @@ def retrieval
                  }
   shipping_manifest["oil paintings"]
 end
-
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
